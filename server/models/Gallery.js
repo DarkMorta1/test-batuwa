a9477f177c6fa9b4c360419c7c9c48c31fff9a5b
DarkMorta1 <163432645+DarkMorta1@users.noreply.github.com>
2026-09-24 13:14:16 +0545

    Delete server directory

--- server/models/Gallery.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const GallerySchema = new mongoose.Schema({
-  path: { type: String, required: true },
-  caption: { type: String, default: '' }
-}, { timestamps: true })
-
-module.exports = mongoose.model('Gallery', GallerySchema)
-
